03ba72a98c82bbd3b11dd47d0ee0b97e6c40bbc8
roonievas <roonievas@gmail.com>
2025-09-02 19:34:21 -0300

    Agregar archivos de proyecto.

---
+++ pryGestionNievas/frmInicio.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace pryGestionNievas
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mrcDatos = new System.Windows.Forms.GroupBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.mrcBusqueda = new System.Windows.Forms.GroupBox();
+            this.cmbCategoría = new System.Windows.Forms.ComboBox();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtCódigo = new System.Windows.Forms.TextBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtDescripción = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.mrcDatos.SuspendLayout();
+            this.mrcBusqueda.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mrcDatos
+            // 
+            this.mrcDatos.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.mrcDatos.Controls.Add(this.btnEliminar);
+            this.mrcDatos.Controls.Add(this.mrcBusqueda);
+            this.mrcDatos.Controls.Add(this.btnModificar);
+            this.mrcDatos.Controls.Add(this.btnAgregar);
+            this.mrcDatos.Controls.Add(this.txtStock);
+            this.mrcDatos.Controls.Add(this.txtPrecio);
+            this.mrcDatos.Controls.Add(this.txtDescripción);
+            this.mrcDatos.Controls.Add(this.txtNombre);
+            this.mrcDatos.Controls.Add(this.lblNombre);
+            this.mrcDatos.Controls.Add(this.lblDescripcion);
+            this.mrcDatos.Controls.Add(this.lblPrecio);
+            this.mrcDatos.Controls.Add(this.lblStock);
+            this.mrcDatos.Location = new System.Drawing.Point(130, 69);
+            this.mrcDatos.Margin = new System.Windows.Forms.Padding(4);
+            this.mrcDatos.Name = "mrcDatos";
+            this.mrcDatos.Padding = new System.Windows.Forms.Padding(4);
+            this.mrcDatos.Size = new System.Drawing.Size(720, 380);
+            this.mrcDatos.TabIndex = 2;
+            this.mrcDatos.TabStop = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(461, 304);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(148, 49);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // mrcBusqueda
+            // 
+            this.mrcBusqueda.Controls.Add(this.cmbCategoría);
+            this.mrcBusqueda.Controls.Add(this.lblCategoria);
+            this.mrcBusqueda.Controls.Add(this.btnBuscar);
+            this.mrcBusqueda.Controls.Add(this.txtCódigo);
+            this.mrcBusqueda.Controls.Add(this.lblCodigo);
+            this.mrcBusqueda.Location = new System.Drawing.Point(43, 20);
+            this.mrcBusqueda.Margin = new System.Windows.Forms.Padding(4);
+            this.mrcBusqueda.Name = "mrcBusqueda";
+            this.mrcBusqueda.Padding = new System.Windows.Forms.Padding(4);
+            this.mrcBusqueda.Size = new System.Drawing.Size(609, 97);
+            this.mrcBusqueda.TabIndex = 12;
+            this.mrcBusqueda.TabStop = false;
+            // 
+            // cmbCategoría
+            // 
+            this.cmbCategoría.FormattingEnabled = true;
+            this.cmbCategoría.Location = new System.Drawing.Point(184, 14);
+            this.cmbCategoría.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbCategoría.Name = "cmbCategoría";
+            this.cmbCategoría.Size = new System.Drawing.Size(187, 24);
+            this.cmbCategoría.TabIndex = 11;
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoria.Location = new System.Drawing.Point(23, 11);
+            this.lblCategoria.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(124, 22);
+            this.lblCategoria.TabIndex = 10;
+            this.lblCategoria.Text = "CATEGORIA";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(427, 32);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(148, 49);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // txtCódigo
+            // 
+            this.txtCódigo.Location = new System.Drawing.Point(184, 57);
+            this.txtCódigo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCódigo.Name = "txtCódigo";
+            this.txtCódigo.Size = new System.Drawing.Size(187, 22);
+            this.txtCódigo.TabIndex = 8;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.Location = new System.Drawing.Point(24, 57);
+            this.lblCodigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(95, 22);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "CÓDIGO";
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(305, 304);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(148, 49);
+            this.btnModificar.TabIndex = 3;
+            this.btnModificar.Text = "MODIFICAR";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(149, 304);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(148, 49);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(569, 255);
+            this.txtStock.Margin = new System.Windows.Forms.Padding(4);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(128, 22);
+            this.txtStock.TabIndex = 9;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(173, 255);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(279, 22);
+            this.txtPrecio.TabIndex = 7;
+            // 
+            // txtDescripción
+            // 
+            this.txtDescripción.Location = new System.Drawing.Point(173, 190);
+            this.txtDescripción.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDescripción.Name = "txtDescripción";
+            this.txtDescripción.Size = new System.Drawing.Size(524, 22);
+            this.txtDescripción.TabIndex = 6;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(173, 129);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(524, 22);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(12, 129);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(91, 22);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "NOMBRE";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(12, 190);
+            this.lblDescripcion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(137, 22);
+            this.lblDescripcion.TabIndex = 3;
+            this.lblDescripcion.Text = "DESCRIPCIÓN";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(12, 255);
+            this.lblPrecio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(80, 22);
+            this.lblPrecio.TabIndex = 2;
+            this.lblPrecio.Text = "PRECIO";
+            // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStock.Location = new System.Drawing.Point(461, 255);
+            this.lblStock.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(70, 22);
+            this.lblStock.TabIndex = 1;
+            this.lblStock.Text = "STOCK";
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 572);
+            this.Controls.Add(this.mrcDatos);
+            this.Name = "frmInicio";
+            this.Text = "Gestión de inventario";
+            this.mrcDatos.ResumeLayout(false);
+            this.mrcDatos.PerformLayout();
+            this.mrcBusqueda.ResumeLayout(false);
+            this.mrcBusqueda.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mrcDatos;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.GroupBox mrcBusqueda;
+        private System.Windows.Forms.ComboBox cmbCategoría;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtCódigo;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtDescripción;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblStock;
+    }
+}
